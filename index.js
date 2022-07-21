3f8ae6b4a80d5ca1cd4cc6efffc8183781fbf254
TejasDhamane11 <109728803+TejasDhamane11@users.noreply.github.com>
2022-07-21 15:13:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,230 @@
-<html>
-<head>
-<title>Javascript Login Form Validation</title>
-<!-- Include CSS File Here -->
-<link rel="stylesheet" href="css/style.css"/>
-<!-- Include JS File Here -->
-<script src="js/login.js"></script>
-</head>
-<body>
-<div class="container">
-<div class="main">
-<h2>Javascript Login Form Validation</h2>
-<form id="form_id" method="post" name="myform">
-<label>User Name :</label>
-<input type="text" name="username" id="username"/>
-<label>Password :</label>
-<input type="password" name="password" id="password"/>
-<input type="button" value="Login" id="submit" onclick="validate()"/>
-</form>
-<span><b class="note">Note : </b>For this demo use following username and password. <br/><b class="valid">User Name : Formget<br/>Password : formget#123</b></span>
-</div>
-</div>
-</body>
-</html>
+cricketList = [
+    {
+      player: "Monster",
+      category: "basket",
+      showLess:false
+    },
+    {
+      player: "Roach",
+      category: "cricket",
+      showLess:false
+    },
+    {
+      player: "Messi",
+      category: "football",
+      showLess:false
+    },
+    {
+      player: "Ms",
+      category: "carrom",
+      showLess:false
+    },
+    {
+      player: "Fedral",
+      category: "tennis",
+      showLess:false
+    }
+        
+  ];
+
+&lt;div style="display: flex;justify-content: space-evenly;"&gt;
+    &lt;div *ngFor="let value of cricketList"&gt;
+        &lt;p (click)='clickOntab(tab)'&gt;{{value.player}}&lt;/p&gt;
+        &lt;button class="staticIcon"   (onShown)="onPopoverShown()" mat-icon-button&gt;
+        &lt;mat-icon *ngIf="!value.showLess" (click)="showResult(value)"&gt;expand_more&lt;/mat-icon&gt;
+            &lt;mat-icon *ngIf="value.showLess" (click)="hideResult(value)"&gt;expand_less&lt;/mat-icon&gt;
+     &lt;/button&gt;
+    &lt;/div&gt;
+&lt;/div&gt;
+
+showResult(param){
+   
+    param.showLess = true;
+  }
+  hideResult(param) {
+    param.showLess = false;
+  }
+
+ ----------------------- 
+cricketList = [
+    {
+      player: "Monster",
+      category: "basket",
+      showLess:false
+    },
+    {
+      player: "Roach",
+      category: "cricket",
+      showLess:false
+    },
+    {
+      player: "Messi",
+      category: "football",
+      showLess:false
+    },
+    {
+      player: "Ms",
+      category: "carrom",
+      showLess:false
+    },
+    {
+      player: "Fedral",
+      category: "tennis",
+      showLess:false
+    }
+        
+  ];
+
+&lt;div style="display: flex;justify-content: space-evenly;"&gt;
+    &lt;div *ngFor="let value of cricketList"&gt;
+        &lt;p (click)='clickOntab(tab)'&gt;{{value.player}}&lt;/p&gt;
+        &lt;button class="staticIcon"   (onShown)="onPopoverShown()" mat-icon-button&gt;
+        &lt;mat-icon *ngIf="!value.showLess" (click)="showResult(value)"&gt;expand_more&lt;/mat-icon&gt;
+            &lt;mat-icon *ngIf="value.showLess" (click)="hideResult(value)"&gt;expand_less&lt;/mat-icon&gt;
+     &lt;/button&gt;
+    &lt;/div&gt;
+&lt;/div&gt;
+
+showResult(param){
+   
+    param.showLess = true;
+  }
+  hideResult(param) {
+    param.showLess = false;
+  }
+
+ ----------------------- 
+cricketList = [
+    {
+      player: "Monster",
+      category: "basket",
+      showLess:false
+    },
+    {
+      player: "Roach",
+      category: "cricket",
+      showLess:false
+    },
+    {
+      player: "Messi",
+      category: "football",
+      showLess:false
+    },
+    {
+      player: "Ms",
+      category: "carrom",
+      showLess:false
+    },
+    {
+      player: "Fedral",
+      category: "tennis",
+      showLess:false
+    }
+        
+  ];
+
+&lt;div style="display: flex;justify-content: space-evenly;"&gt;
+    &lt;div *ngFor="let value of cricketList"&gt;
+        &lt;p (click)='clickOntab(tab)'&gt;{{value.player}}&lt;/p&gt;
+        &lt;button class="staticIcon"   (onShown)="onPopoverShown()" mat-icon-button&gt;
+        &lt;mat-icon *ngIf="!value.showLess" (click)="showResult(value)"&gt;expand_more&lt;/mat-icon&gt;
+            &lt;mat-icon *ngIf="value.showLess" (click)="hideResult(value)"&gt;expand_less&lt;/mat-icon&gt;
+     &lt;/button&gt;
+    &lt;/div&gt;
+&lt;/div&gt;
+
+showResult(param){
+   
+    param.showLess = true;
+  }
+  hideResult(param) {
+    param.showLess = false;
+  }
+
+ ----------------------- 
+&lt;div style="display: flex;justify-content: space-evenly;"&gt;
+    &lt;div *ngFor="let value of cricketList; let i = index"&gt;
+        &lt;p (click)='clickOntab(tab)' &gt;{{value.player}}&lt;/p&gt;
+    &lt;button class="staticIcon" (onShown)="onPopoverShown()" (click)="toggleResult(i)" mat-icon-button&gt;
+        &lt;mat-icon *ngIf="value.showMore"&gt;expand_more&lt;/mat-icon&gt;
+        &lt;mat-icon *ngIf="!value.showMore"&gt;expand_less&lt;/mat-icon&gt;
+     &lt;/button&gt;  
+    &lt;/div&gt;
+&lt;/div&gt;
+
+   cricketList = [
+    {
+      player: "Monster",
+      category: "basket",
+      showMore: false
+    },
+    {
+      player: "Roach",
+      category: "cricket",
+      showMore: false
+    },
+    {
+      player: "Messi",
+      category: "football",
+      showMore: false
+    },
+    {
+      player: "Ms",
+      category: "carrom",
+      showMore: false
+    },
+    {
+      player: "Fedral",
+      category: "tennis",
+      showMore: false
+    }
+        
+  ];
+
+  toggleResult(i) {
+    this.cricketList[i].showMore = !this.cricketList[i].showMore;
+  }
+
+ ----------------------- 
+&lt;div style="display: flex;justify-content: space-evenly;"&gt;
+    &lt;div *ngFor="let value of cricketList; let i = index"&gt;
+        &lt;p (click)='clickOntab(tab)' &gt;{{value.player}}&lt;/p&gt;
+    &lt;button class="staticIcon" (onShown)="onPopoverShown()" (click)="toggleResult(i)" mat-icon-button&gt;
+        &lt;mat-icon *ngIf="value.showMore"&gt;expand_more&lt;/mat-icon&gt;
+        &lt;mat-icon *ngIf="!value.showMore"&gt;expand_less&lt;/mat-icon&gt;
+     &lt;/button&gt;  
+    &lt;/div&gt;
+&lt;/div&gt;
+
+   cricketList = [
+    {
+      player: "Monster",
+      category: "basket",
+      showMore: false
+    },
+    {
+      player: "Roach",
+      category: "cricket",
+      showMore: false
+    },
+    {
+      player: "Messi",
+      category: "football",
+      showMore: false
+    },
+    {
+      player: "Ms",
+      category: "carrom",
+      showMore: false
+    },
+    {
+      player: "Fedral",
+      category: "tennis",
+      showMore: false
+    }
+        
+  ];
+
+  toggleResult(i) {
+    this.cricketList[i].showMore = !this.cricketList[i].showMore;
+  }
